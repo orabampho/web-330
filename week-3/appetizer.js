a14e66d323f71f3577d1d60a7c1936ee5b30964c
orabampho <orabampho@outlook.com>
2022-01-22 21:54:57 -0500

    edit codes and replace errors

--- week-3/appetizer.js
+++ week-3/appetizer.js
@@ -8,7 +8,6 @@ Description: Create app to select items and calculate
 total price using JS import/export
 Source:  
 Material Design Icons - "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-How to Toggle - https://www.w3schools.com/howto/howto_js_toggle_like.asp
 ====================================================
 */
 

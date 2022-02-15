bab2f7faf7ced1f922f1f338858e6ecdbb8c0c0d
orabampho <orabampho@outlook.com>
2022-02-15 16:43:10 -0500

    edit css style

--- rabampho-portfolio/footer.js
+++ rabampho-portfolio/footer.js
@@ -12,10 +12,9 @@ class Footer extends HTMLElement
                     <li><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Bellevue University Web Development Degree</a></li>
                     <li><a href="https://www.youtube.com/c/bellevueuniversity">Bellevue University YouTube Channel</a></li>
                     <li><a href="https://github.com/buwebdev/web-330">Web 330: JavaScript II Course GitHub Repository</a></li>
+                    <br/>
+                    <li id="copyright">Copyright &copy; 2022 Rabampho</li>
                 </ul>
-                <p id="copyright">
-                    Copyright &copy; 2022 RABAMPHO
-                    </p>
             </div>
         
     `;
